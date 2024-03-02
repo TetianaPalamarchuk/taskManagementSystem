f6c6a5ccf52a11ab6ea8a6f54c4208a5cf828f75
diel <diel-d@web.de>
2024-03-02 16:09:17 +0100

    update Services - допилил сервис createPerson - Валидатор пока закомментирован.

--- src/main/java/org/taskmanagementsystem/service/UserServiceImpl.java
+++ src/main/java/org/taskmanagementsystem/service/UserServiceImpl.java
@@ -3,36 +3,105 @@ import org.taskmanagementsystem.dto.DTO;
 import org.taskmanagementsystem.dto.StringDTO;
 import org.taskmanagementsystem.entity.Person;
 import org.taskmanagementsystem.repository.PersonRepository;
-import org.taskmanagementsystem.repository.PersonRepositoryImpl;
-
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class UserServiceImpl implements UserService {
 // Validator validator;
         private PersonRepository persons;
-
-
-public UserServiceImpl(PersonRepository persons /* Validator v */) {
+public UserServiceImpl(PersonRepository persons /* ,Validator v */) {
         this.persons = persons;
 //        validator = v;
 }
-
 @Override
 public List<DTO> createPerson(DTO dto) {
-
-//        List<> rc = validator.validate(dto)
-//        if(rc.size() == 0) ...
-
-        Person newPerson = new Person(dto.get("name"), dto.get("email"), dto.get("pass"));
-        persons.addPerson(newPerson);
-        DTO response = new StringDTO();
-
-        response.set("status", "Ok");
+//      List<DTO> validationErrors = validator.validate(dto);
         List<DTO> dtoList = new ArrayList<>();
-        dtoList.add(response);
-
+//      if(validationErrors.isEmpty()) {
+        Person newPerson = new Person(dto.get("name"), dto.get("email"), dto.get("pass"));
+                if (persons.addPerson(newPerson)){
+                DTO response = new StringDTO();
+                response.set("status", "Ok");
+                dtoList.add(response);}
+                else {
+                DTO response = new StringDTO();
+                response.set("status", "Add person to database failed");
+                dtoList.add(response);}
+//        } return validationErrors;
 
         return dtoList;
+        }
+public List<DTO> loginPerson(DTO dto){
+//      List<DTO> validationErrors = validator.validate(dto);
+        List<DTO> dtoList = new ArrayList<>();
+//      if(validationErrors.isEmpty()) {
+
+        DTO response = new StringDTO();
+        response.set("status", "Ok");
+        dtoList.add(response);
+//        } return validationErrors;
+        return dtoList;
 }
+public List<DTO> addPersonToProject(DTO dto){
+//      List<DTO> validationErrors = validator.validate(dto);
+        List<DTO> dtoList = new ArrayList<>();
+//      if(validationErrors.isEmpty()) {
+
+        DTO response = new StringDTO();
+        response.set("status", "Ok");
+        dtoList.add(response);
+//        } return validationErrors;
+        return dtoList;
+}
+public List<DTO> addPersonToTask(DTO dto){
+//      List<DTO> validationErrors = validator.validate(dto);
+        List<DTO> dtoList = new ArrayList<>();
+//      if(validationErrors.isEmpty()) {
+
+
+        DTO response = new StringDTO();
+        response.set("status", "Ok");
+        dtoList.add(response);
+//        } return validationErrors;
+        return dtoList;
+}
+private List<DTO> findPersonByName(DTO dto){
+//      List<DTO> validationErrors = validator.validate(dto);
+        List<DTO> dtoList = new ArrayList<>();
+//      if(validationErrors.isEmpty()) {
+
+
+        DTO response = new StringDTO();
+        response.set("status", "Ok");
+        dtoList.add(response);
+//        } return validationErrors;
+        return dtoList;
+
+}
+private List<DTO> findPersonByID(DTO dto){
+//      List<DTO> validationErrors = validator.validate(dto);
+        List<DTO> dtoList = new ArrayList<>();
+//      if(validationErrors.isEmpty()) {
+
+
+        DTO response = new StringDTO();
+        response.set("status", "Ok");
+        dtoList.add(response);
+//        } return validationErrors;
+        return dtoList;
+}
+private List<DTO> findPersonByEMail(DTO dto){
+//      List<DTO> validationErrors = validator.validate(dto);
+        List<DTO> dtoList = new ArrayList<>();
+//      if(validationErrors.isEmpty()) {
+
+
+        DTO response = new StringDTO();
+        response.set("status", "Ok");
+        dtoList.add(response);
+//        } return validationErrors;
+        return dtoList;
+}
+
 }
